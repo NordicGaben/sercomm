7766331e592726eef90f0af0ea48ca0eef284898
Gabriel Voluson <gabe@dng.is>
2026-09-01 15:12:20 +0000

    stdlib inclusion posix Linux/Max
    Was having compilation issues on linux machine, consuming this library

--- sercomm/posix/dev_linux.c
+++ sercomm/posix/dev_linux.c
@@ -25,6 +25,7 @@
 #include "public/sercomm/dev.h"
 
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
--- sercomm/posix/dev_macos.c
+++ sercomm/posix/dev_macos.c
@@ -24,6 +24,7 @@
 
 #include "public/sercomm/dev.h"
 
+#include <stdlib.h>
 #include <limits.h>
 #include <pthread.h>
 
